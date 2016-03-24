c840ac39f32c00880c38d9b9ab6188efd77c69b9
Jack <slowsay@gmail.com>
2016-03-24 23:09:33 +0800

    Added files via upload

---
+++ example/js/app/base.js
@@ -0,0 +1,60 @@
+/**
+ * @author slowsay
+ * @description 翻页功能
+ */
+define(function(require) {
+    var _wid = $(window).width(), _hei = $(window).height();
+    var data = [{
+        id : 0,
+        title : 'jack',
+        time : '12'
+    }, {
+        id : 0,
+        title : 'jack',
+        time : '12'
+    }, {
+        id : 0,
+        title : 'jack',
+        time : '12'
+    }, {
+        id : 0,
+        title : 'jack',
+        time : '12'
+    }, {
+        id : 0,
+        title : 'jack',
+        time : '12'
+    }, {
+        id : 0,
+        title : 'jack',
+        time : '12'
+    }, {
+        id : 0,
+        title : 'jack',
+        time : '12'
+    }, {
+        id : 1,
+        title : 'slowsay',
+        time : '12'
+    }, {
+        id : 2,
+        title : 'pp',
+        time : '12'
+    }, {
+        id : 3,
+        title : 'ww',
+        time : '12'
+    }, {
+        id : 4,
+        title : 'dd',
+        time : '12'
+    }];
+    /**
+     * @method 翻页功能
+     */
+    var flipPage = require('flipPage');
+    $('body').append('<div id=viewcon></div><div id=viewcon2></div>');
+    var _pp = new flipPage('#viewcon', data, 6);
+    var _kk = new flipPage('#viewcon2', data, 3);
+
+});
